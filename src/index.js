f875c4b44d2fd00f5ce0290ce027aa47b931dea1
adam4ever <adam4ever@gmail.com>
2020-01-15 20:53:06 -0400

    Refactor App layout. Update core.
    Add Normalize.
    Add Prop-Types
    Config @babel/preset-react
    Update .gitignore

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import '../src/scss/style.scss';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import '../node_modules/modern-normalize/modern-normalize.css';
 
-class Welcome extends React.Component {
-  render() {
-    return (
-      <div>
-        <em>Astonishing Tales from Space!</em>
-        <h1>Weird Future</h1>
-        <strong>CTA</strong>
-      </div>
-    );
-  }
-}
-
-ReactDom.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
